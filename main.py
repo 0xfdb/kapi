8465a721271315bda7088cc1e9fb64d6ea9ce446
aida <aida@0xfdb.xyz>
2020-06-14 14:32:00 -0400

    refactor, force config, add style

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
+"""Usage: main.py [CONFIGURATION FILE]
+"""
+
+import configparser
 import json
-import os
+import sys
 from difflib import get_close_matches
+from typing import NoReturn, Optional
 
 import cherrypy
+
 from kodipydent import Kodi
 
 KODI = None
 
 
-class KodiServ(object):
-    try:
-        os.environ["KODI_KEY"]
-    except KeyError:
-        cherrypy.log("KODI_KEY not set")
-        key = "qZu3vhYicbJA87Xq2bofnwQMyQ"
-    else:
-        key = os.environ["KODI_KEY"]
+class KodiServ:
+    def __init__(self, key: str, style: str):
+        self.key: str = key
+        self.style: str = style
 
     def can_request(self) -> bool:
         header = cherrypy.request.headers
@@ -26,12 +28,12 @@ class KodiServ(object):
             return False
 
     @staticmethod
-    def auth_failed():
+    def auth_failed() -> NoReturn:
         raise cherrypy.HTTPError(401, message="Authentication failed")
 
     # commands
     @cherrypy.expose
-    def play(self, title=None, id=None):
+    def play(self, title: Optional[str] = None, id: Optional[int] = None) -> NoReturn:
         if self.can_request() and (title is not None or id is not None):
             movies = self.getmovies()
             # TODO comparator9000()
@@ -42,71 +44,62 @@ class KodiServ(object):
             KODI.Player.Open(item={"movieid": id})
 
     @cherrypy.expose
-    def pause(self):
+    def pause(self) -> NoReturn:
         if self.can_request():
             KODI.Player.PlayPause(1)
 
     @cherrypy.expose
-    def playpause(self):
+    def playpause(self) -> NoReturn:
         if self.can_request():
             KODI.Player.PlayPause(1)
 
     @cherrypy.expose
-    def stop(self):
+    def stop(self) -> NoReturn:
         if self.can_request():
             KODI.Player.Stop()
 
     # info
     @cherrypy.expose
-    def movies(self):
+    def movies(self) -> str:
         movies = self.getmovies()
-        fmt = "{movieid:<4} {label}\n"
-        text="ID    Title\n{:-^35}\n".format("-")
+        page = """
+        <html><head><title>Movie List</title>
+        <style>{style}</style></head>
+        <table><tr><th>ID</th><th>Title</th></tr>
+        """.format(
+            style=self.style
+        )
+        entryline = "<tr><td>{movieid}</td><td>{label}</td></tr>\n"
         for movie in movies:
-            text += fmt.format(**movie)
-        return text
+            page += entryline.format(**movie)
+        page += "</table>\n</html>"
+        return page
 
     @cherrypy.expose
-    def nowplaying(self):
+    @cherrypy.tools.json_out()
+    def nowplaying(self) -> str:
         info = KODI.Player.GetItem(playerid=1, properties=[])
-        print(info)
         if info["result"]["item"]["label"] != "":
             details: dict = self.getmoviedetails(info["result"]["item"]["id"])
         else:
             details: dict = self.getmoviedetails(None)
 
-        return json.dumps(details)
+        return details
 
-    def getmoviedetails(self, movieid) -> dict:
-        fmt = {
-            "active": True,
-            "imdbnumber": "",
-            "lastplayed": "",
-            "movieid": "",
-            "title": "",
-            "streamdetails": "",
-        }
+    def getmoviedetails(self, movieid: str) -> dict:
         if movieid is None:
-            fmt["title"] = "Nothing is playing"
-            fmt["active"] = False
+            return {"active": False, "title": "Nothing is playing"}
         else:
             details = KODI.VideoLibrary.GetMovieDetails(
                 movieid=movieid,
                 properties=["title", "imdbnumber", "lastplayed", "streamdetails"],
             )
-            # AHHH FUNCTIONAL PLZ
-            _ = details["result"]["moviedetails"]
-            fmt["imdbnumber"] = _["imdbnumber"]
-            fmt["lastplayed"] = _["lastplayed"]
-            fmt["movieid"] = _["movieid"]
-            fmt["title"] = _["title"]
-            fmt["streamdetails"] = _["streamdetails"]
-        return fmt
+            details["result"]["moviedetails"]["active"] = True
+        return details["result"]["moviedetails"]
 
     def getmovies(self) -> dict:
-        _ = KODI.VideoLibrary.GetMovies()
-        movies = _["result"]["movies"]
-        return movies
+        query = KODI.VideoLibrary.GetMovies()
+        return query["result"]["movies"]
 
     def comparator9000(self, string: str) -> list:
         movie_list = []
@@ -115,28 +108,35 @@ class KodiServ(object):
         return get_close_matches(string, movie_list)
 
 
-
 if __name__ == "__main__":
-    config = {
-        "server.socket_host": "127.0.0.1",
-        "server.socket_port": 8989,
-        "request.show_tracebacks": False,
-        "tools.response_headers.on": True,
-        "tools.response_headers.headers": [("Content-Type", "text/plain")]
-    }
-    cherrypy.config.update(config)
     try:
-        KODI = Kodi("localhost")
-    except Exception as err:
-        reason = str(err.reason)
-        cherrypy.log("Failed to load Kodi")
-        cherrypy.log(reason)
-
-        import sys
-
-        sys.exit("Failed to load Kodi\n" + reason)
+        kapiconfig = sys.argv[1]
+    except IndexError:
+        print("Missing configuration file")
+        sys.exit(__doc__)
     else:
-        try:
-            cherrypy.quickstart(KodiServ())
-        except Exception as err:
-            sys.exit(err)
+        config = configparser.ConfigParser()
+        config.read(kapiconfig)
+        cherrypyconfig = {
+            "server.socket_host": config["Server"]["host"],
+            "server.socket_port": config.getint("Server", "port"),
+            "request.show_tracebacks": config.getboolean(
+                "Server", "debug", fallback=False
+            ),
+            "tools.response_headers.on": True,
+            "tools.response_headers.headers": [("Content-Type", "text/html")],
+        }
+        cherrypy.config.update(cherrypyconfig)
+    try:
+        KODI = Kodi(
+            hostname=config["Kodi"]["hostname"],
+            port=config["Kodi"]["port"],
+            username=config.get("Kodi", "username", fallback=None),
+            password=config.get("Kodi", "password", fallback=None),
+        )
+    except Exception as err:
+        cherrypy.log(str(err.reason))
+        sys.exit(f"Failed to load Kodi\n{str(err.reason)}")
+    else:
+        style = config.get("Server", "style")
+        cherrypy.quickstart(KodiServ(key=config["Server"]["key"], style=style))
